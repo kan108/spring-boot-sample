3ecc471f3b5483faddd7e3c17779cc9c17707a99
Kan038 <test@aaa.com>
2016-02-24 06:58:17 +0900

    初期データ作成SQL変更

--- src/main/java/data.sql
+++ src/main/java/data.sql
@@ -1,4 +1,4 @@
-insert into customers(first_name, last_name) values('Nobita','Nobi');
-insert into customers(first_name, last_name) values('Takeshi','Goda');
-insert into customers(first_name, last_name) values('Suneo','Honekawa');
-insert into customers(first_name, last_name) values('Shizuka','Minamoto');
+INSERT INTO customers(first_name, last_name) VALUES('Nobita', 'Nobi');
+INSERT INTO customers(first_name, last_name) VALUES('Takeshi', 'Goda');
+INSERT INTO customers(first_name, last_name) VALUES('Suneo', 'Honekawa');
+INSERT INTO customers(first_name, last_name) VALUES('Shizuka', 'Minamoto');
