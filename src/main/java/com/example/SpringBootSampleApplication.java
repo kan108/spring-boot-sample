966084710aaa1f1132f7ff7a0dcdd1b8f732180e
Kan038 <test@aaa.com>
2016-02-24 07:33:57 +0900

    Polishing.

--- src/main/java/com/example/SpringBootSampleApplication.java
+++ src/main/java/com/example/SpringBootSampleApplication.java
@@ -1,10 +1,24 @@
 package com.example;
 
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@MapperScan(basePackages="com.example.test.domain.repository")	// Mapperの自動検出　(MyBatis-Spring1.2.0以降）
+@SpringBootApplication	// @Configuration、@EnableAutoConfiguration、@ComponentScanを設定したことと同じ意味
 public class SpringBootSampleApplication {
+	
+	@Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        return sessionFactory;
+    }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
