e64be7a12603cae7bc0aa63c148ccfe18e32e31d
Kan038 <test@aaa.com>
2016-02-25 07:11:34 +0900

    mybatis-config.xmlの追加

--- src/main/java/com/example/SpringBootSampleApplication.java
+++ src/main/java/com/example/SpringBootSampleApplication.java
@@ -7,18 +7,21 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 
-@MapperScan(basePackages="com.example.test.domain.repository")	// Mapperの自動検出　(MyBatis-Spring1.2.0以降）
-@SpringBootApplication	// @Configuration、@EnableAutoConfiguration、@ComponentScanを設定したことと同じ意味
+@MapperScan(basePackages = "com.example.test.domain.repository") // Mapperの自動検出
+																	// (MyBatis-Spring1.2.0以降）
+@SpringBootApplication // @Configuration、@EnableAutoConfiguration、@ComponentScanを設定したことと同じ意味
 public class SpringBootSampleApplication {
-	
+
 	@Bean
-    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        return sessionFactory;
-    }
-	
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		sessionFactory.setConfigLocation(new ClassPathResource("/META-INF/mybatis/mybatis-config.xml"));
+
+		return sessionFactory;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
